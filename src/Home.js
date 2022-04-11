6bd2e4fd5ab44e257f9a1420d9bb141c4b3bec3b
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-04-11 16:31:02 -0300

    begin project climate

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import './Home.css'
+import Corpo from './components/Corpo';
+
+
+function Home() {
+  return (
+      <Corpo />
+  );
+}
+
+export default Home;
---
+++ src/components/Corpo.jsx
@@ -0,0 +1,14 @@
+import logo from '../logo.png'
+
+export default function Corpo() {
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <div>
+        <input type="text" />
+        <button>Buscar</button>
+      </div>
+    </div>
+  )
+}
+
